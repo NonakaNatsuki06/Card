c5bb2fec269aa7d044c08677f7813a8d3ea09eb6
NonakaNatsuki06 <n-nonaka@shikaku-square.com>
2019-06-20 17:07:44 +0900

    6/20

---
+++ js/script.js
@@ -0,0 +1,35 @@
+
+$(function() {
+  var card_num = _.random(0, 11);
+  $('#1').click(function() {
+    $(this).attr('src', 'img/No_' + card_num + '.png');
+    });
+  });
+
+
+$(function() {
+  var card_num = _.random(0, 11);
+  $('#2').click(function() {
+    $(this).attr('src', 'img/No_' + card_num + '.png');
+    });
+  });
+
+
+$(function() {
+  var card_num = _.random(0, 11);
+  $('#3').click(function() {
+    $(this).attr('src', 'img/No_' + card_num + '.png');
+    });
+  });
+
+
+
+
+
+
+
+
+
+
+
+
